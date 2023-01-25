812fcb5c466062a524a0bcfb0c75ae965bb2763d
Rogpi <rogpigames@gmail.com>
2023-01-25 14:48:33 -0400

    StartGame and Exit Methods
    Adding the custom methods for the Start Button and Quit Button

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MenuUI : MonoBehaviour
 {
@@ -14,5 +18,19 @@ public class MenuUI : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
